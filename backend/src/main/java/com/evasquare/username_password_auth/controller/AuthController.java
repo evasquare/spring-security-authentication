9ca007fce656615ec28b2a96f0be16d9694b2e7a
evasquare <177620651+evasquare@users.noreply.github.com>
2025-03-05 11:16:52 +0900

    Refactor code

--- backend/src/main/java/com/evasquare/username_password_auth/controller/AuthController.java
+++ backend/src/main/java/com/evasquare/username_password_auth/controller/AuthController.java
@@ -1,30 +1,33 @@
 package com.evasquare.username_password_auth.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.evasquare.username_password_auth.entity.UserEntity;
 import com.evasquare.username_password_auth.models.JoinModel;
 import com.evasquare.username_password_auth.models.LoginModel;
-import com.evasquare.username_password_auth.service.JoinService;
+import com.evasquare.username_password_auth.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/auth")
+@AllArgsConstructor
 public class AuthController {
-    @Autowired
-    private JoinService joinService;
+    private final AuthenticationManager authenticationManager;
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginModel loginModel, HttpSession session) {
@@ -46,11 +49,19 @@ public class AuthController {
     }
 
     @PostMapping("/join")
-    public String join(@RequestBody JoinModel requestBody) {
-        System.out.println(requestBody.getUsername());
-        joinService.joinProcess(requestBody);
+    public ResponseEntity<String> join(@RequestBody JoinModel requestBody) {
+        boolean isUser = userRepository.existsByUsername(requestBody.getUsername());
+        if (isUser) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists");
+        }
 
-        return new String();
+        UserEntity data = new UserEntity();
+        data.setUsername(requestBody.getUsername());
+        data.setPassword(bCryptPasswordEncoder.encode(requestBody.getPassword()));
+        data.setRole("ROLE_USER");
+        userRepository.save(data);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Join Successful!");
     }
 
     @GetMapping("/get-username")
