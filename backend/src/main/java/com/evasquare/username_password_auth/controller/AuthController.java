3c49f78718b06a40989b0c67418ca0ee71d7f26d
evasquare <177620651+evasquare@users.noreply.github.com>
2025-03-07 19:00:06 +0900

    Remove unused import

--- backend/src/main/java/com/evasquare/username_password_auth/controller/AuthController.java
+++ backend/src/main/java/com/evasquare/username_password_auth/controller/AuthController.java
@@ -2,8 +2,6 @@ package com.evasquare.username_password_auth.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
